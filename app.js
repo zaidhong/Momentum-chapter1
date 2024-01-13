9ec8d590a4450581d5b6b142caf9a11d89ecd144
zaidhong <78392521+zaidhong@users.noreply.github.com>
2024-01-13 09:47:34 +0900

    classList, toggle
    [Update] clicked, classList(remove, add, contains, toggle) function

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ document.h1 = 'Javascript-Basic';
 const h1 = document.querySelector('.hello:first-child h1');
 
 function handleClick() {
-    const clickedClass = 'clicked';
-    if(h1.className === clickedClass) h1.className = '';
-    else h1.className = clickedClass;
+    /* const clickedClass = 'clicked';
+    if(h1.classList.contains(clickedClass)) h1.classList.remove(clickedClass)
+    else h1.classList.add(clickedClass) => toggle */
+    h1.classList.toggle('clicked')
 }
 
 h1.addEventListener('click', handleClick);
